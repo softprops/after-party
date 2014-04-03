e380d30d00d48af40736a5a17af38c048fb26c8c
softprops <d.tangren@gmail.com>
2014-04-03 08:37:28 -0400

    run => start

--- README.md
+++ README.md
@@ -6,4 +6,17 @@ define: [webhook](https://help.github.com/articles/post-receive-hooks). a place 
 * irc notifier
 * ...
 
+
+# usage
+
+create a server. start a party
+
+```scala
+import afterparty._
+Server(port).start(AfterParty.empty.onPush { push =>
+  println(
+    s"${push.pusher.name} pushed ${commits.size} commits to ${push.ref}")
+})
+```
+
 Doug Tangren (softprops) 2014
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -5,14 +5,14 @@ import unfiltered.request.Path
 
 object Server {
   def main(args: Array[String]) {
-    Server(4567).run(AfterParty.empty.onPush {
+    Server(4567).start(AfterParty.empty.onPush {
       push => println(s"got pushed $push")
     })
   }
 }
 
 case class Server(port: Int, path: String = "/") {
-  def run(after: AfterParty) =
+  def start(after: AfterParty) =
     netty.Http(port).handler(netty.async.Planify {
       case req @ Path(path) => after.intent(req)
     }).run()
